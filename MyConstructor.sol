7f4f2a30b56ea238ce82326e0581b16807115a55
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-10-30 21:29:12 +0000

    docs: added comment for constructor example

--- MyConstructor.sol
+++ MyConstructor.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 contract MyConstructor {
     address public myAddress;
 
+    // constructor is only called once and only once during deployment
     constructor(address _someAddress) {
         myAddress = _someAddress;
     }
