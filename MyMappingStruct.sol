d6192d076d899b8d86e1f28aeb10358fe3536d84
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-11-01 15:21:50 +0000

    feat: simple mapping struct wallet example

---
+++ MyMappingStruct.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract MappingsStructExample {
+    struct Transaction {
+        uint amount;
+        uint timestamp;
+    }
+
+    struct Balance {
+        uint totalBalance;
+        uint numDeposits;
+        mapping(uint => Transaction) deposits;
+        uint numWithdrawals;
+        mapping(uint => Transaction) withdrawals;
+    }
+
+    mapping(address => Balance) public balanceReceived;
+
+    function getBalance(address _addr) public view returns(uint) {
+        return balanceReceived[_addr].totalBalance;
+    }
+
+    function depositMoney() public payable {
+        balanceReceived[msg.sender].totalBalance += msg.value;
+
+        Transaction memory deposit = Transaction(msg.value, block.timestamp);
+        balanceReceived[msg.sender].deposits[balanceReceived[msg.sender].numDeposits] = deposit;
+        balanceReceived[msg.sender].numDeposits++;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public payable {
+        // reduce balance by the amount of withdraw
+        balanceReceived[msg.sender].totalBalance -= _amount;
+
+        // record new withdrawal
+        Transaction memory withdrawal = Transaction(msg.value, block.timestamp);
+        balanceReceived[msg.sender].withdrawals[balanceReceived[msg.sender].numWithdrawals] = withdrawal;
+        balanceReceived[msg.sender].numWithdrawals++;
+
+        // transfer amount
+        _to.transfer(_amount);
+    }
+}
